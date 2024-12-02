7d278157334ceb6e79c43a473b2c4ca328e010de
Chad Walker <chad@cwalker.dev>
2024-12-01 23:25:36 -0600

    day 2

--- aoc2402.ts
+++ aoc2402.ts
@@ -4,16 +4,64 @@ type AdditionalInfo = {
     [key: string]: string;
 };
 
+type Report = number[];
+function isSafe(report: Report): boolean {
+    let isDecreasing = false;
+    if (report.length >= 2) {
+        isDecreasing = report[0] > report[1];
+    }
+    let safe = true;
+    for (let i = 1; i < report.length; i++) {
+        if (isDecreasing) {
+            if (report[i-1] <= report[i] || (report[i-1] - report[i]) > 3) {
+                safe = false;
+                break;
+            }
+        } else {
+            if (report[i-1] >= report[i] || (report[i] - report[i-1]) > 3) {
+                safe = false;
+                break;
+            }
+        }
+    }
+    return safe;
+}
+
 export async function solve(
     input: string[],
     part: number,
     test: boolean,
     additionalInfo?: AdditionalInfo,
 ): Promise<string | number> {
-    if (part === 1) {
-        throw new NotImplemented('Not Implemented');
+    const reports: Report[] = [];
+    for (const line of input) {
+        reports.push(line.split(/\s+/g).map(i => parseInt(i)))
     }
-    throw new NotImplemented('Not Implemented');
+    if (part === 1) {
+        let safe = 0;
+        for (const report of reports) {
+            if (isSafe(report)) {
+                safe++;
+            }
+        }
+        return safe;
+    }
+    let safe = 0;
+    for (const report of reports) {
+        if (isSafe(report)) {
+            safe++;
+            continue;
+        }
+        for (let i = 0; i < report.length; i++) {
+            const subReport = report.slice();
+            subReport.splice(i, 1);
+            if (isSafe(subReport)) {
+                safe++;
+                break;
+            }
+        }
+    }
+    return safe;
 }
 
 run(__filename, solve);

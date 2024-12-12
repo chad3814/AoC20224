7921cf09d391c37ce5f2bcd5d8dc1de6a71f889b
Chad Walker <chad@cwalker.dev>
2024-12-12 08:20:04 -0600

    day 12 part 2, left-hand-rule

--- aoc2412.ts
+++ aoc2412.ts
@@ -41,33 +41,133 @@ function getPerimeter(plot: GraphNode<string>[]): number {
     return count;
 }
 
-function dKey(direction: Direction): 'north'|'east'|'south'|'west' {
+function dForward(direction: Direction): 'north'|'east'|'south'|'west' {
     if (direction === Direction.NORTH) return 'north';
     if (direction === Direction.EAST) return 'east';
     if (direction === Direction.SOUTH) return 'south';
     return 'west';
 }
 
-function getSides(node: GraphNode<string>): number {
-    let sides = 1;
-    let direction = Direction.EAST;
-    const startX = node.x;
-    const startY = node.y;
-    let x = node.x;
-    let y = node.y;
+function dLeft(direction: Direction): 'north'|'east'|'south'|'west' {
+    if (direction === Direction.NORTH) return 'west';
+    if (direction === Direction.EAST) return 'north';
+    if (direction === Direction.SOUTH) return 'east';
+    return 'south';
+}
 
-    do {
-        while(node[dKey(direction)]) {
-            node = node[dKey(direction)]!;
-            x = node.x;
-            y = node.y;
+function dRight(direction: Direction): 'north'|'east'|'south'|'west' {
+    if (direction === Direction.NORTH) return 'east';
+    if (direction === Direction.EAST) return 'south';
+    if (direction === Direction.SOUTH) return 'west';
+    return 'north';
+}
+
+function turnLeft(direction: Direction): Direction {
+    if (direction === Direction.NORTH) return Direction.WEST;
+    if (direction === Direction.EAST) return Direction.NORTH;
+    if (direction === Direction.SOUTH) return Direction.EAST;
+    return Direction.SOUTH;
+}
+
+function turnRight(direction: Direction): Direction {
+    if (direction === Direction.NORTH) return Direction.EAST;
+    if (direction === Direction.EAST) return Direction.SOUTH;
+    if (direction === Direction.SOUTH) return Direction.WEST;
+    return Direction.NORTH;
+}
+
+function dIndex(direction: Direction): 0|1|2|3 {
+    if (direction === Direction.NORTH) return 0;
+    if (direction === Direction.EAST) return 1;
+    if (direction === Direction.SOUTH) return 2;
+    return 3;
+}
+
+function decCount<T>(map: Map<T,number>, key: T): void {
+    const value = map.get(key);
+    if (value == null) {
+        throw new Error('key missing');
+    }
+    if (value == 0) {
+        throw new Error('0 value');
+    }
+    map.set(key, value - 1);
+}
+
+function getSides(nodes: GraphNode<string>[]): number {
+    let sides = 0;
+    let direction = Direction.EAST;
+
+    const visitsNeeded = new Map<string, [boolean, boolean, boolean, boolean]>();
+    for (const n of nodes) {
+        visitsNeeded.set(n.value, [
+            !!n[dForward(Direction.NORTH)],
+            !!n[dForward(Direction.EAST)],
+            !!n[dForward(Direction.SOUTH)],
+            !!n[dForward(Direction.WEST)],
+        ]);
+    }
+
+    function pickNode(): GraphNode<string>|null {
+        let target: GraphNode<string>|null = null;
+        for (const [key, needed] of visitsNeeded.entries()) {
+            target = nodes.find(
+                n => n.value === key
+            ) ?? null;
+            if (target == null) {
+                throw new Error('key not found');
+            }
+
+            if (!target[dForward(Direction.NORTH)]) {
+                continue;
+            }
+            return target;
         }
-        direction = direction === Direction.EAST ?
-            Direction.SOUTH : direction === Direction.SOUTH ?
-            Direction.WEST : direction === Direction.WEST ?
-            Direction.NORTH : Direction.EAST;
-        sides++;
-    } while(x !== startX || y !== startY || direction !== Direction.EAST);
+        return null;
+    }
+
+    let node: GraphNode<string>|null = null;
+    while(node = pickNode()) {
+        console.log('picked', node.value);
+        const startX = node.x;
+        const startY = node.y;
+        let x = node.x;
+        let y = node.y;
+
+        do {
+            node.visited = true;
+            while(node[dForward(direction)] && !node[dLeft(direction)]) {
+                node = node[dForward(direction)]!;
+
+                const needed = visitsNeeded.get(node.value);
+                if (!needed) {
+                    throw new Error;
+                }
+                needed[dIndex(direction)] = false;
+                visitsNeeded.set(node.value, needed);
+
+                x = node.x;
+                y = node.y;
+            }
+            if (node[dLeft(direction)]) {
+                direction = turnLeft(direction);
+                node = node[dForward(direction)]!;
+
+                const needed = visitsNeeded.get(node.value);
+                if (!needed) {
+                    throw new Error;
+                }
+                needed[dIndex(direction)] = false;
+                visitsNeeded.set(node.value, needed);
+
+                x = node.x;
+                y = node.y;
+            } else {
+                direction = turnRight(direction);
+            }
+            sides++;
+        } while(x !== startX || y !== startY || direction !== Direction.EAST);
+    }
     return sides;
 }
 
@@ -110,13 +210,14 @@ export async function solve(
     let cost = 0;
     for (const plot of map.values()) {
         const area = getArea(plot);
-        const sides = getSides(plot[0]);
+        const sides = getSides(plot);
         console.log(plot[0].value, sides);
         cost += area * sides;
     }
     return cost;
 }
 
+// run(__filename, solve);
 run(__filename, solve, undefined, {
     reason: 'multiple',
     part1length: 3,

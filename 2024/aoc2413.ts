3234b565d0d066d67e3d74cfb3cd131d98e8b2c8
Chad Walker <chad@cwalker.dev>
2024-12-14 19:21:28 -0600

    add additional test for day 13 part 2

--- 2024/aoc2413.ts
+++ 2024/aoc2413.ts
@@ -87,15 +87,26 @@ export async function solve(
     return cost.toString(10);
 }
 
-run(__filename, solve, {testsOnly: true}, {
-    "reason": "Multiple examples",
-    "part1length": 1,
-    "inputs": {
-        "selector": "code",
-        "indexes": [8, 8]
-    },
-    "answers": {
-        "selector": "code",
-        "indexesOrLiterals": [43, "875318608908"]
+run(__filename, solve, undefined, undefined, [
+    {
+        part: 2,
+        inputs: [
+            'Button A: X+94, Y+34',
+            'Button B: X+22, Y+67',
+            'Prize: X=8400, Y=5400',
+            '',
+            'Button A: X+26, Y+66',
+            'Button B: X+67, Y+21',
+            'Prize: X=12748, Y=12176',
+            '',
+            'Button A: X+17, Y+86',
+            'Button B: X+84, Y+37',
+            'Prize: X=7870, Y=6450',
+            '',
+            'Button A: X+69, Y+23',
+            'Button B: X+27, Y+71',
+            'Prize: X=18641, Y=10279'
+        ],
+        answer: '875318608908'
     }
-});
+]);

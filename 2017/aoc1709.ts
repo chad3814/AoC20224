969c38b17942b14e721c91a6bfeaa963ffa105a1
Chad Walker <chad@cwalker.dev>
2025-01-24 10:57:08 -0600

    solved 2017 day 9

--- 2017/aoc1709.ts
+++ 2017/aoc1709.ts
@@ -4,16 +4,55 @@ type AdditionalInfo = {
     [key: string]: string;
 };
 
+type Garbage = {
+    str: string;
+};
+
+type Group = {
+    data: (Group|Garbage)[];
+};
+
 export async function solve(
     input: string[],
     part: number,
     test: boolean,
     additionalInfo?: AdditionalInfo,
 ): Promise<string | number> {
-    if (part === 1) {
-        throw new NotImplemented('Not Implemented');
+    const stream = input[0].split('');
+    let score = 0;
+    let level = 0;
+    let inGarbage = false;
+    let garbageCount = 0;
+    for (let i = 0; i < stream.length; i++) {
+        const c = stream[i];
+        if (!inGarbage && c === '<') {
+            inGarbage = true;
+            continue;
+        }
+        if (inGarbage && c === '>') {
+            inGarbage = false;
+            continue;
+        }
+        if (c === '!') {
+            i++;
+            continue;
+        }
+        if (inGarbage) {
+            garbageCount++;
+            continue;
+        }
+        if (c === '{') {
+            level++;
+            score += level;
+        }
+        if (c === '}') {
+            level--;
+        }
     }
-    throw new NotImplemented('Not Implemented');
+    if (part === 1) {
+        return score;
+    }
+    return garbageCount;
 }
 
 run(__filename, solve);

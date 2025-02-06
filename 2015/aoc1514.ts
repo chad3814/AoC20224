2a44f228ef4d062290cfeafd0201f334a5e71fa1
Chad Walker <chad@cwalker.dev>
2025-02-06 15:48:50 -0600

    2015 day 14

--- 2015/aoc1514.ts
+++ 2015/aoc1514.ts
@@ -1,7 +1,17 @@
-import { NotImplemented, Options, run } from "aoc-copilot";
+import { AdditionalInfo, NotImplemented, Options, run } from "aoc-copilot";
 
-type AdditionalInfo = {
-    [key: string]: string;
+// Vixen can fly 8 km/s for 8 seconds, but then must rest for 53 seconds
+const RE = /(?<name>\w+) can fly (?<rate>\d+) km\/s for (?<fly>\d+) .*? rest for (?<rest>\d+)/u;
+
+type Reindeer = {
+    name: string;
+    rate: number;
+    fly: number;
+    rest: number;
+    distance: number;
+    score: number;
+    isFlying: boolean;
+    countDown: number;
 };
 
 export async function solve(
@@ -10,10 +20,45 @@ export async function solve(
     test: boolean,
     additionalInfo?: AdditionalInfo,
 ): Promise<string | number> {
+    const reindeer: Reindeer[] = input.filter(i => i !== '').map(
+        line => {
+            const match = line.match(RE);
+            if (!match || !match.groups) {
+                throw new Error('RE failed ' + line);
+            }
+            const name = match.groups.name;
+            const rate = parseInt(match.groups.rate, 10);
+            const fly = parseInt(match.groups.fly, 10);
+            const rest = parseInt(match.groups.rest, 10);
+            return {name, rate, fly, rest, distance: 0, score: 0, isFlying: true, countDown: fly};
+        }
+    );
+    const time = test ? 1000 : 2503;
     if (part === 1) {
-        throw new NotImplemented('Not Implemented');
+        return Math.max(...reindeer.map(
+            deer => {
+                const deerTime = deer.fly + deer.rest;
+                const count = Math.floor(time / deerTime);
+                const dist = count * deer.rate * deer.fly;
+                return dist + Math.min(deer.fly, time % deerTime) * deer.rate;
+            }
+        ));
     }
-    throw new NotImplemented('Not Implemented');
+    for (let t = 0; t < time; t++) {
+        for (const deer of reindeer) {
+            deer.countDown--;
+            if (deer.isFlying) deer.distance += deer.rate;
+            if (deer.countDown === 0) {
+                deer.countDown = deer.isFlying ? deer.rest : deer.fly;
+                deer.isFlying = !deer.isFlying;
+            }
+        }
+        const max = Math.max(...reindeer.map(d => d.distance));
+        for (const deer of reindeer) {
+            if (deer.distance === max) deer.score++;
+        }
+    }
+    return Math.max(...reindeer.map(d => d.score));
 }
 
 const options: Options = {};
